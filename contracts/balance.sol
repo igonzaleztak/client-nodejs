399ce7fccb9d6e77a7e20ecc8164ffc065363505
Ivan <igonzaleztak@gmail.com>
2020-10-14 14:24:53 +0200

    u

--- contracts/balance.sol
+++ contracts/balance.sol
@@ -1,6 +1,13 @@
 pragma solidity >=0.4.0 <= 0.6.1;
 
 
+// Definition of the functions of the other contract that are going to be required
+// in this contract
+contract dataLedgerContract 
+{
+    function retrieveInfo(bytes32) public view returns (string memory, string memory) {}
+}
+
 contract balanceContract 
 {
     address admin = 0x21A018606490C031A8c02Bb6b992D8AE44ADD37f;
@@ -17,8 +24,11 @@ contract balanceContract
         // Array that stores all the subscriptions
         bytes32[] arraySubs;
     
-        // mappin byte32 to index array
+        // mapping byte32 to index array
         mapping(bytes32 => uint) indexArraySubs;
+    
+        // Array with the free indexes
+        uint[] arrayIndexes;
     }
     
     // Struct that holds the available Subscriptions
@@ -27,8 +37,20 @@ contract balanceContract
         // Map that holds all the suscriptions
         mapping(bytes32 => bool) allSubs;
         
+        // Map that stores all the subscribers
+        mapping(bytes32 => address[]) userSubscriptions;
+        
+        // Map that stores the index of the previous Array
+        mapping(bytes32 => mapping(address => uint)) userSubscriptionsIndex;
+        
+        // Array that stores free indexes
+        mapping(bytes32 => uint[]) freeIndexes;
+        
         // Array that holds the ids available
         bytes32[] availableTypes;
+        
+        // Array that holds the free indexes of the previous Array
+        uint[] availableTypesIndexes;
         
         // mappin byte32 to index array
         mapping(bytes32 => uint) indexAvailableTypes;
@@ -54,6 +76,13 @@ contract balanceContract
     // Event to notify the removal of categories
     event notifyRemoveCategory(address indexed _addr, bytes32 indexed _name);
     
+    // Event to notify the sending of the tokens
+    event sendTokenEvent(address indexed _addr, bytes32 indexed _hash, bytes32 indexed _txHash);
+    
+    // Event to awnser the token sent by the platform
+    event anwserTokenEvent(bytes32 indexed _hash, bytes32 indexed _txHash);
+    
+    
     /****************** Greeting ****************************/
     function greet() pure public returns(string memory)
     {
@@ -68,17 +97,59 @@ contract balanceContract
         // Check whether the requested subscription category exists
         require(subs.allSubs[subName] == true, "The requested category does not exist");
         
+        // Check if the subscription has expired.
+        //  - True: subscription has expired. Therefore, delete the existing subscription
+        //  - False: Subscription has not expired or does not exist
+        if (usersSubs[msg.sender].subscriptions[subName] > now)
+        {
+            deleteSub(subName, msg.sender);
+        }
+        
         // Check whether the user is already subscribed
         require(usersSubs[msg.sender].allSubs[subName] == false, "The user is already subscribe to that type");
         
         // Create the suscription in the user struct
         usersSubs[msg.sender].allSubs[subName] = true;
         usersSubs[msg.sender].subscriptions[subName] = now + time;
-        usersSubs[msg.sender].arraySubs.push(subName);
-        usersSubs[msg.sender].indexArraySubs[subName] = usersSubs[msg.sender].arraySubs.length;
+        
+        // Check whether there is a free index
+        if (usersSubs[msg.sender].arrayIndexes.length == 0) 
+        {
+            usersSubs[msg.sender].arraySubs.push(subName);
+            usersSubs[msg.sender].indexArraySubs[subName] = usersSubs[msg.sender].arraySubs.length;
+        }
+        else 
+        {
+            uint newIndex = usersSubs[msg.sender].arrayIndexes[usersSubs[msg.sender].arrayIndexes.length];
+            usersSubs[msg.sender].arraySubs[newIndex] = subName;
+            usersSubs[msg.sender].indexArraySubs[subName] = newIndex;
+            
+            // The index is no longer free. Therefore, it must be removed from the 
+            // the array that holds the free indexes.
+            delete usersSubs[msg.sender].arrayIndexes[usersSubs[msg.sender].arrayIndexes.length];
+        }
+
         
         // Add the values of the suscription in the subsStruct
         subs.allSubs[subName] = true;
+        
+        // Check whether there is a free index in the array
+        if (subs.freeIndexes[subName].length == 0)
+        {
+            subs.userSubscriptions[subName].push(msg.sender);
+            subs.userSubscriptionsIndex[subName][msg.sender] = subs.userSubscriptions[subName].length;
+        }
+        else 
+        {
+            uint newIndex2 = subs.freeIndexes[subName][subs.freeIndexes[subName].length];
+            subs.userSubscriptions[subName][newIndex2] = msg.sender;
+            subs.userSubscriptionsIndex[subName][msg.sender] = newIndex2;
+            
+            // The index is no longer free. Therefore, it must be removed from the 
+            // the array that holds the free indexes.
+            delete subs.freeIndexes[subName][subs.freeIndexes[subName].length];
+        }
+        
         
         // Emit event
         emit notifyNew(msg.sender, subName, now + time);
@@ -86,13 +157,40 @@ contract balanceContract
     
     
     // Endpoint used by costumer to delete subscriptions
-    function deleteSub(bytes32 subName) public 
+    function deleteSub(bytes32 subName, address clientAddr) public 
     {
+        // The admin can delete subscriptions if they have expired
+        if (msg.sender == admin)
+        {
+            // Check whether the user is subscribed
+            require(usersSubs[clientAddr].allSubs[subName] == true, "The user is not suscribed to the requeste type");
+            
+            // Check whether the susbcription has expired
+            require(now > usersSubs[msg.sender].subscriptions[subName], "Subscription is still active");
+            
+            // Get index of Subscription
+            uint _index = usersSubs[clientAddr].indexArraySubs[subName];
+            
+            // remove sub from userStruct
+            delete usersSubs[clientAddr].allSubs[subName];
+            delete usersSubs[clientAddr].arraySubs[_index];
+            
+            // Remove sub from subs
+            delete subs.allSubs[subName];
+            uint _indexSub = subs.userSubscriptionsIndex[subName][clientAddr];
+            subs.freeIndexes[subName].push(_indexSub);
+            delete subs.userSubscriptions[subName][_indexSub];
+            delete subs.userSubscriptionsIndex[subName][clientAddr];
+            
+            // emit event
+            emit notifyRemove(clientAddr, subName);
+        }
+        
         // Check whether the user is subscribed
         require(usersSubs[msg.sender].allSubs[subName] == true, "The user is not suscribed to the requeste type");
         
         // Check whether the susbcription is still active
-        require(now < usersSubs[msg.sender].subscriptions[subName], "Subscription is not active");
+        // require(now < usersSubs[msg.sender].subscriptions[subName], "Subscription is not active");
         
         // Get index of Subscription
         uint _index = usersSubs[msg.sender].indexArraySubs[subName];
@@ -103,9 +201,15 @@ contract balanceContract
         
         // Remove sub from subs
         delete subs.allSubs[subName];
+        uint _indexSub = subs.userSubscriptionsIndex[subName][msg.sender];
+        subs.freeIndexes[subName].push(_indexSub);
+        delete subs.userSubscriptions[subName][_indexSub];
+        delete subs.userSubscriptionsIndex[subName][msg.sender];
         
         // emit event
         emit notifyRemove(msg.sender, subName);
+        
+        return;
     }
     
     
@@ -120,8 +224,23 @@ contract balanceContract
         
         // Add the category
         subs.allSubs[subName] = true;
-        subs.availableTypes.push(subName);
-        subs.indexAvailableTypes[subName] = subs.availableTypes.length;
+        
+        // Check whether there is a free index available
+        if (subs.availableTypesIndexes.length == 0) 
+        {
+            subs.availableTypes.push(subName);
+            subs.indexAvailableTypes[subName] = subs.availableTypes.length;
+        }
+        else 
+        {
+            uint _index = subs.availableTypesIndexes[subs.availableTypesIndexes.length];
+            subs.availableTypes[_index] = subName;
+            subs.indexAvailableTypes[subName] = _index;
+            
+            delete subs.availableTypesIndexes[subs.availableTypesIndexes.length];
+        }
+        
+
         emit notifyNewCategory(msg.sender, subName);
     }
     
@@ -143,17 +262,65 @@ contract balanceContract
     }
     
     
-    //getAllSubscriptions shows all the suscriptions that the user is subscribed to
-    function getAllSubscriptions() public view returns (bytes32[] memory)
+    // Check subscription status
+    function checkSubStatus(bytes32 subName, address clientAddr) public returns (bool, uint)
     {
-        return usersSubs[msg.sender].arraySubs;
+        // Only admin user and the customer who originated the subscription can see its status
+        if (msg.sender == admin)
+        {
+            // If the subscription has expired delete it
+            if (usersSubs[clientAddr].subscriptions[subName] > now)
+            {
+                deleteSub(subName, clientAddr);
+            }
+            return (usersSubs[clientAddr].allSubs[subName], usersSubs[clientAddr].subscriptions[subName]);
+        }
+        
+        // If the subscription has expired delete it
+        if (usersSubs[msg.sender].subscriptions[subName] > now)
+        {
+            deleteSub(subName, msg.sender);
+        }
+        return (usersSubs[msg.sender].allSubs[subName], usersSubs[msg.sender].subscriptions[subName]);
+    }
+    
+    // Get all available types 
+    function getAllTypes() public view returns (bytes32[] memory)
+    {
+        return subs.availableTypes;
     }
     
     
-    // getAllTopics gets all the topics available in the platform
-    function getAllTopics() public view returns (bytes32[] memory) 
+    // Checks type status
+    function checkType(bytes32 typeName) public view returns (bool)
     {
-        return subs.availableTypes;
+        return subs.allSubs[typeName];
+    }
+    
+    
+    // Gets all the accounts subsribed to a type
+    function getSubsToType(bytes32 subName) public view returns (address[] memory)
+    {
+        require(msg.sender == admin, "You do not have enough privileges to do this action");
+        return subs.userSubscriptions[subName];
+    }
+    
+    
+    // Send token to client. 
+    // Admin uses this function to send tokens to the client.
+    function sendToken(bytes32 txHash, address to, bytes32 hash) public 
+    {
+        // Only the admin address can create tokens
+        require(msg.sender == admin, "You do not have enough privileges to do this action");
+        emit sendTokenEvent(to, hash, txHash);
+    }
+    
+    
+    // Anwser to token. 
+    // Customers use this function to anwser back with the signed token.
+    function anwserToken(bytes32 hash, bytes32 txHash) public 
+    {
+        emit anwserTokenEvent(hash, txHash);
     }
     
 }
--- tests/test.js
+++ tests/test.js
@@ -1,47 +1,246 @@
 const fs = require('fs')
 const Wallet = require('ethereumjs-wallet');
 const crypto = require('crypto');
+const secp256k1 = require('secp256k1');
 const eciesjs = require('eciesjs');
 const Web3 = require('web3');
 const net = require('net');
 
+const axios = require('axios');
+const https = require('https');
+const blockchain = require('../libs/blockchain.js');
+const { Eth } = require('web3-eth');
+
 
 
 /***************** Global variables *********************/
-const gethPath = '/home/ivan/Desktop/demoPOA2/new-node/geth.ipc';
+const gethPath = '/home/ivan/Desktop/demoPOA2/client-node/geth.ipc';
 const web3 = new Web3(gethPath, net);
 
-const clientAddr = "0x5bab040bc593f57eda64ea431b14f182fe167f3f";
+const clientAddr = "0x9ebbb19c78b9a553c837af8e13037856aec76b6d";
+const password = "1";
+
+const X ="696f742d736d61727473616e74616e6465723100000000000000000000000000";
+const folder = '/home/ivan/Desktop/demoPOA2/client-node/keystore/';
+
+// The following line is to accept TLS connections with self-signed certificates
+const instance = axios.create({
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  })
+});
 
 /******************** Functions ************************/
+/**
+ * Converts Uint8Array to hex string
+ * @param {Uint8Array} arrayBuffer 
+ */
+function buf2hex(arrayBuffer) 
+{
+  let buff = new Uint8Array(arrayBuffer);
+  return Buffer.from(buff).toString('hex');
+}
+
+/**
+ * Parses text from format1('from') to format2('to')
+ * E.g: to parseFormat string to hex
+ *       hex = parseFormat(text, 'utf8', 'hex')
+ * @param {String} text 
+ * @param {String} from 
+ * @param {String} to 
+ * @return {String}
+ */
+function parseFormat(text, from, to) {
+  return Buffer.from(text, from).toString(to);
+}
+
+
+/**
+ * Send signed transaction to contract method
+ * @param {Web3} web3 
+ * @param {ContractTransaction} transaction 
+ * @param {String} privKey 
+ */
+async function sendTransactionContract(web3, transaction, privKey)
+{
+  let options =  
+  {
+    to: 			transaction._parent._address,
+    data: 		transaction.encodeABI(),
+    gasPrice: '0',
+    gas:	4000000
+  };
+
+  let signedTransaction = await web3.eth.accounts.signTransaction(options, privKey);
+  let receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
+  .catch((err) => {
+    throw(err);
+  });
+  return receipt;
+}
+
+/**
+ * Get the file, which contains the parameters to extract the
+ * private key of an ethereum account.
+ * @param {String} path 
+ * @param {String} pattern
+ * @return {String} 
+ */
+function getFilesFromPath(path, pattern)
+{
+  let dir = fs.readdirSync(path);
+  return dir.filter( fn => fn.match(pattern));
+}
 
 /**
  * Gets the private key of the client
  * @param {String} password
  * @return {String} private key and public key
  */
-function getClientKeys(password) {
-  // File in which the elements to obtain the private key are stored
-  let utcFile = "producer-keystore";
-  let clientWallet = Wallet.fromV3(fs.readFileSync(utcFile).toString(), password, true);
+async function getClientKeys(password) {
+  // Get the UTC file that has the parameters to extract the 
+  // private key 
+  let pattern = new RegExp(".*" + clientAddr.substring(2), "i");
+  let utcFile = "" + folder + getFilesFromPath(folder, pattern)[0];
+
+  // Get the wallet associated to the client's account
+  let clientWallet = await Wallet.default.fromV3(fs.readFileSync(utcFile).toString(), password, true);
 
   return [clientWallet.getPrivateKey().toString('hex'), clientWallet.getPublicKey().toString('hex')]
 }
 
+
+/**
+ * Extract the Token from an event
+ * @param {Web3} web3 
+ * @param {Event} event 
+ * @return {String} tokenString
+ */
+async function processEvent(web3, event)
+{
+  // Get transaction hash from the event
+  let txHash = event.returnValues._txhahs;
+  //TODO: Change indexed parameter name in Balance SC
+
+  // Get the token from the transaction
+  let tx = await web3.eth.getTransaction(txHash);
+  let tokenString = web3.utils.hexToUtf8(tx.input);
+
+  return tokenString;
+}
+
+
+/**
+ * Signs the hash of the message
+ * @param {String} message 
+ * @param {String} privateKey 
+ * @returns {Object} hash signed
+ */
+function ecdsaSign(message, privateKey) 
+{
+  // Hash the message and sign it
+  let hash = crypto.createHash('SHA256').update(message).digest(); 
+  let sig = secp256k1.ecdsaSign(hash, Buffer.from(privateKey, 'hex'))
+  return sig;
+}
+
+
+/**
+ * Cashes the token in the measurement broker and, as a result of this,
+ * obtains the value of the measurement
+ * @param {Object} tokenObject 
+ */
+async function postToken(tokenObject)
+{
+  let returnedValue;
+  let bearerToken = parseFormat(JSON.stringify(tokenObject), 'utf8', 'base64');
+
+  await instance.get(tokenObject.url, {headers: { Authorization : 'Bearer ' + bearerToken}})
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {console.log(err.response.data);});
+}
+
+/**
+ * Listens the events emmitted by the Balance Contract
+ * @param {Web3} web3 
+ * @param {BalanceContract} balanceContract 
+ */
+async function listenToSubs(web3, balanceContract, privKey)
+{
+  let filter = {
+    _addr: web3.utils.toChecksumAddress(clientAddr)
+  };
+  let i = 1;
+  balanceContract.events.sendTokenEvent({filter}, async function(error, event) {
+    if (error) throw(error);
+    console.log("\nEvent %d\n", i);
+    i++;
+
+    // Get the token from the input of the transaction
+    let tokenString = await processEvent(web3,event)
+
+    // Sign the token to cash it in the measurement Broker
+    let tokenSignature = (ecdsaSign(tokenString, privKey)).signature;
+
+    // Parse the token string to Object
+    let tokenObj = JSON.parse(tokenString);
+
+    // Add the signature to the token
+    tokenObj.signature = "0x" + buf2hex(tokenSignature);
+
+    // Post the token to the measurements broker
+    await postToken(tokenObj);
+  });
+}
+
+
 async function main() 
 {
+  console.log("Testing client");
+  
+  // Unlock account: Get private key
+  let [clientPrivKey, clientPubKey] = await getClientKeys(password);
 
-  // Get the private key of the client
-  let [privKey, pubKey] = getClientKeys("1")
+  // Balance Contract
+  const balanceContract = await blockchain.initContract(web3
+    , blockchain.balanceABI
+    , blockchain.balanceContractAddress);
 
-  // Message to encrypt
-  let msg = "1"
+  // Get all types available in Balance SC
+  let allTypes = await balanceContract.methods.getAllTypes().call();
 
-  let encrypted = eciesjs.encrypt(pubKey, Buffer.from(msg));
-  console.log(encrypted.toString('hex'));
+  console.log(allTypes);
 
-  let plain = eciesjs.decrypt(privKey, encrypted);
-  console.log(plain)
+  // Check wheter the user is already subscribed to the type
+  let [isSubscribed, expirationDate] = await balanceContract.methods.checkSubStatus(Buffer.from(X, 'hex'), web3.utils.toChecksumAddress(clientAddr)).call({
+    from: clientAddr
+  });
+
+  if (!isSubscribed || expirationDate < Date.now()) 
+  {
+    await sendTransactionContract(web3, balanceContract.methods.subscribeTo(Buffer.from(X, 'hex'), 3600), clientPrivKey)
+    .catch((err) => {
+      throw(err);
+    });
+    console.log("client was successfully subscribed");
+  }
+
+
+  // Subscribe to one type(X)
+  /*
+  await sendTransactionContract(web3, balanceContract.methods.subscribeTo(Buffer.from(X, 'hex'), 3600), clientPrivKey)
+  .catch((err) => {
+    throw(err);
+  });
+  console.log("client was successfully subscribed");
+  */
+  
+  console.log(status);
+
+  // Listen to subscriptions
+  await listenToSubs(web3, balanceContract, clientPrivKey);
 
 }
 
